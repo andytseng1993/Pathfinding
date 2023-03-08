d99ab93f66622112b9f5d7f0cb13c17cf06a0e33
Andy Tseng <anby1611@gmail.com>
2023-03-08 13:32:06 -0800

    Build paint wall fn

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -1,8 +1,69 @@
 import classes from './Node.module.css'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 
-const Node = ({ path }) => {
+const Node = ({
+	path,
+	handleChangeNode,
+	paintWallBtn,
+	paintWall,
+	setPaintWall,
+}) => {
+	const handleClick = (row, col) => {
+		const isStart = false
+		const isFinish = true
+		handleChangeNode(row, col, isStart, isFinish)
+	}
+	const handlePaintWallStart = () => {
+		if (!paintWallBtn) return
+		setPaintWall(true)
+	}
+	const handleMove = (row, col, isWall) => {
+		if (isWall) return
+		if (!paintWallBtn) return
+		if (!paintWall) return
+		handleChangeNode(row, col, false, false, true)
+	}
+	const handlePaintWallEnd = () => {
+		if (!paintWallBtn) return
+		setPaintWall(false)
+	}
+	console.log(1)
 	return path.map((col, index) => (
-		<div key={index} className={classes.node}></div>
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{
+				opacity: 1,
+				transition: {
+					duration: 0.5,
+					delay: 0.1 + index / 30,
+				},
+			}}
+			transition={{ duration: 0.3 }}
+			viewport={{ once: true }}
+			whileHover={{
+				scale: 1.1,
+				transition: { duration: 0.1 },
+			}}
+			whileTap={{
+				scale: 0.9,
+				transition: { duration: 0.1 },
+			}}
+			key={index}
+			className={`${classes.node} ${
+				col.isStart
+					? classes.start
+					: col.isFinish
+					? classes.finish
+					: col.isWall
+					? classes.wall
+					: ''
+			}`}
+			onClick={() => handleClick(col.row, col.col)}
+			onMouseDown={handlePaintWallStart}
+			onMouseMove={() => handleMove(col.row, col.col, col.isWall)}
+			onMouseUp={handlePaintWallEnd}
+		></motion.div>
 	))
 }
 
--- src/pages/Path.jsx
+++ src/pages/Path.jsx
@@ -1,29 +1,91 @@
 import { useEffect, useState } from 'react'
 import Node from '../components/Node'
 import classes from './Path.module.css'
+import { motion } from 'framer-motion'
 
 const Path = () => {
 	const [pathes, setPathes] = useState([])
+	const [paintWallBtn, setPaintWallBtn] = useState(false)
+	const [paintWall, setPaintWall] = useState(false)
+
 	useEffect(() => {
 		let nodes = []
 		for (let row = 0; row < 15; row++) {
 			let currentRow = []
 			for (let col = 0; col < 50; col++) {
-				currentRow.push([])
+				const currentNode = {
+					row,
+					col,
+					isStart: row === 8 && col === 5,
+					isFinish: row === 8 && col === 45,
+					isWall: false,
+				}
+				currentRow.push(currentNode)
 			}
 			nodes.push(currentRow)
 		}
 		setPathes(nodes)
 	}, [])
-
+	const handleChangeNode = (
+		row,
+		col,
+		isStart = false,
+		isFinish = false,
+		isWall = false
+	) => {
+		let newPathes = [...pathes]
+		const newNode = {
+			row,
+			col,
+			isStart,
+			isFinish,
+			isWall,
+		}
+		newPathes[row].splice(col, 1, newNode)
+		setPathes(newPathes)
+	}
+	const handlePaintWall = () => {
+		setPaintWallBtn(!paintWallBtn)
+	}
+	const handleStopPainting = () => {
+		setPaintWall(false)
+	}
 	return (
 		<>
 			<h1>Path Finding</h1>
-			<div>
+			<button onClick={handlePaintWall}>
+				{paintWallBtn ? 'Painting wall..' : 'Paint wall'}
+			</button>
+			<div
+				style={{
+					display: 'block',
+					width: '1700px',
+					marginLeft: 'auto',
+					marginRight: 'auto',
+				}}
+				onMouseLeave={handleStopPainting}
+			>
 				{pathes.map((path, index) => (
-					<div key={index} className={classes.row}>
-						<Node path={path} />
-					</div>
+					<motion.div
+						key={index}
+						className={classes.row}
+						initial={{ opacity: 0 }}
+						animate={{
+							opacity: 1,
+							transition: {
+								duration: 0.5,
+								delay: 0.1 + index / 20,
+							},
+						}}
+					>
+						<Node
+							path={path}
+							handleChangeNode={handleChangeNode}
+							paintWallBtn={paintWallBtn}
+							paintWall={paintWall}
+							setPaintWall={setPaintWall}
+						/>
+					</motion.div>
 				))}
 			</div>
 		</>
